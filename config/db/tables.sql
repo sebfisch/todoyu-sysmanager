378e6a1666132d0f8731b8c6cd8f96aa7eb53e21
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2009-12-13 10:46:32 +0000

    * further refinements to installer / update script

--- config/db/tables.sql
+++ config/db/tables.sql
@@ -6,6 +6,7 @@ CREATE TABLE `ext_sysmanager_extension` (
   `id` int(11) NOT NULL auto_increment,
   `date_update` int(10) unsigned NOT NULL,
   `date_create` int(10) unsigned NOT NULL default '0',
-  `version` text NOT NULL,
+  `ext` smallint(5) unsigned NOT NULL,
+  `version` varchar(16) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
