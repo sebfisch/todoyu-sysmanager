6317b8ca489acc4fc012f3bc75e1b49cc96c91df
nkarrer <nkarrer@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-04-29 11:33:46 +0000

    * changed pk + fk in db to int(10)

--- config/db/tables.sql
+++ config/db/tables.sql
@@ -3,10 +3,10 @@
 --
 
 CREATE TABLE `ext_sysmanager_extension` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `date_create` int(10) unsigned NOT NULL default '0',
   `date_update` int(10) unsigned NOT NULL,
-  `ext` smallint(5) unsigned NOT NULL,
+  `ext` int(10) unsigned NOT NULL,
   `version` varchar(16) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
