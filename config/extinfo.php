d70dc4319a28d8afcb8e47a8d75677d41654297e
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-08-11 11:32:34 +0000

    * Updated version of core to 2.0.3 and extensions to 1.0.3

--- config/extinfo.php
+++ config/extinfo.php
@@ -31,7 +31,7 @@ Todoyu::$CONFIG['EXT']['sysmanager']['info'] = array(
 		'company'	=> 'snowflake productions GmbH, Zurich'
 	),
 	'state' 		=> 'stable',
-	'version'		=> '1.0.2',
+	'version'		=> '1.0.3',
 	'constraints' => array(
 		'depends' => array(
 
