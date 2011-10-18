72ab988633572fb2bc1f60c0d2ac606c099cb5ff
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2011-10-18 08:50:55 +0000

    * Translation: Polish

--- config/extinfo.php
+++ config/extinfo.php
@@ -31,7 +31,7 @@ Todoyu::$CONFIG['EXT']['sysmanager']['info'] = array(
 		'company'	=> 'snowflake productions GmbH, Zurich'
 	),
 	'state' 		=> 'stable',
-	'version'		=> '1.1.2',
+	'version'		=> '1.1.3',
 	'constraints' 	=> array(
 		'depends' 	=> array(),
 		'conflicts' => array(),
