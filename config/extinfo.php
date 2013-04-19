8836fb5694f068f82977bd7efa22a1f673f14837
nkarrer <nkarrer@7f067039-fbb2-4aac-a504-d491c780d6a3>
2013-04-19 07:22:52 +0000

    * Bugfix: Set selection of person to lazy init (role-record; caused a memory_limit_exhausted error)

--- config/extinfo.php
+++ config/extinfo.php
@@ -31,7 +31,7 @@ Todoyu::$CONFIG['EXT']['sysmanager']['info'] = array(
 		'company'	=> 'snowflake productions GmbH, Zurich'
 	),
 	'state'			=> 'stable',
-	'version'		=> '1.3.0',
+	'version'		=> '1.3.1',
 	'constraints'	=> array(
 		'core'		=> '2.3.0',
 		'system'	=> true,
