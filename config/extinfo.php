27822fae1c617f529adf4387ed0da43948262c75
nkarrer <nkarrer@7f067039-fbb2-4aac-a504-d491c780d6a3>
2013-12-30 16:14:26 +0000

    * Added: release informations

--- config/extinfo.php
+++ config/extinfo.php
@@ -3,7 +3,7 @@
 * todoyu is published under the BSD License:
 * http://www.opensource.org/licenses/bsd-license.php
 *
-* Copyright (c) 2012, snowflake productions GmbH, Switzerland
+* Copyright (c) 2013, snowflake productions GmbH, Switzerland
 * All rights reserved.
 *
 * This script is part of the todoyu project.
@@ -33,7 +33,7 @@ Todoyu::$CONFIG['EXT']['sysmanager']['info'] = array(
 	'state'			=> 'stable',
 	'version'		=> '1.3.1',
 	'constraints'	=> array(
-		'core'		=> '2.3.0',
+		'core'		=> '2.3.1',
 		'system'	=> true,
 		'conflicts' => array(
 			'admin'	=> '1.1.2'
